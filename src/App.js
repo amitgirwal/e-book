5128affae8ef18d3aa9f5a69f9c27a95b54ed546
Ankur Saxena <ankursaxenaankur0@gmail.com>
2021-10-28 23:24:32 +0530

    home screen added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import reactDom from "react-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import Header1 from "./components/header 1";
-import Header2 from "./header 2";
+import Header2 from "./components/header 2";
+import Home from "./components/homejs";
 
 function App(){
-    console.log(window.innerWidth);
+    console.log(window.innerHeight);
     return(
         <div>
             <Header1 />
+            <Home />
         </div>
     )
 }
